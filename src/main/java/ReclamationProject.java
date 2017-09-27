f1d36fac1d39464c6c017fb79bbca54878e9c910
Administrator <Administrator@79Z8IWVZILIZ0HO>
2017-09-27 10:48:12 -0500

    awoihf

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,49 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ * oajfiwejip.
+ * @param
+ * @return
+ * @see
+ */
+public class ReclamationProject {
+    /**
+     * oajfiwejip.
+     * @param originalA is string
+     * @param originalB is string
+     * @return nothing
+     * @see
+     */
+    static String doit(final String originalA, final String originalB) {
+        String a = originalA, b = originalB;
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+        if (a.length() > b.length()) {
+            String c = a; // set c to a
+            a = b; b = c;
+            }
+        String r;
+        if (a.equals(b)) {
+            r = "";
+        } else {
+            r = "";
+        }
+        // I love the ternary operator!
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    boolean firstCondition = a.regionMatches(i, b, k, j);
+                    if (firstCondition) {
+                        if (j > r.length()) {
+                            a.substring(i, i + j);
+                        }
+                    }
+                        }
+            } // Ah yeah
+        }
+        return r;
+        }
 }
